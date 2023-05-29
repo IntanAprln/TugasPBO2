a7ea31cd15c1e5525d99125c01a90c257b5c7641
IntanAprln <infointanapriliana@gmail.com>
2023-05-29 22:32:42 +0800

    menghapus comment

--- src/App.java
+++ src/App.java
@@ -4,8 +4,9 @@ import com.sun.net.httpserver.HttpServer;
 public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(1087), 0);
-        server.createContext("/", new MyHandler()); // Menetapkan handler ke path tertentu
-        server.setExecutor(null); // Menggunakan executor default
-        server.start(); // Memulai server
+        server.createContext("/", new MyHandler()); 
+        server.setExecutor(null); 
+        server.start(); 
     }
+
 }
